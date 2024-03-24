5d1af4fa04997c61505b81cf02b7df304bdf12b0
Ali t.nazari <alitabatabaee20@gmail.com>
2024-03-24 23:12:37 +0330

    :tada:

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Heading, Mark } from 'flowbite-svelte';
+	import { DarkMode } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
+	import { GithubSolid } from 'flowbite-svelte-icons';
+</script>
+
+<header class="my-10 select-none text-center">
+	<Heading tag="h1" class="text-3xl">
+		<Mark>Magic</Mark> CSS 🧙‍♂️
+	</Heading>
+</header>
+<div class="grid grid-cols-1 justify-items-center">
+	<section>
+		<Button
+			color="light"
+			href=""
+			class="rounded-lg bg-transparent p-2 text-xl text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+			><GithubSolid /></Button
+		>
+		<Button
+			size="sm"
+			color="light"
+			href=""
+			class="rounded-lg bg-transparent p-2 text-xl text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+			><DarkMode class="p-0" /></Button
+		>
+	</section>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.pcss';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<Footer />
