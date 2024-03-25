15d5fbde0beee9f1874a2fbc0da813a028085f99
Ali t.nazari <alitabatabaee20@gmail.com>
2024-03-25 13:24:45 +0330

    :sparkles: you can now create your own text-shadow property

---
+++ src/routes/text-shadow/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import ColorPicker, { ChromeVariant } from 'svelte-awesome-color-picker';
+	import Rslider from '../../components/UI/Rslider.svelte';
+	import Code from '../../components/Code.svelte';
+	let hex = '#2563eb';
+	let x = { min: -130, max: 130, values: [0] };
+	let y = { min: -130, max: 130, values: [0] };
+	let blur = { min: 0, max: 20, values: [10] };
+	$: textShadowValue = `text-shadow: ${x.values[0]}px ${y.values}px ${blur.values[0]}px ${hex}`;
+</script>
+
+<main class="container mt-10 grid grid-cols-1 gap-2 text-center lg:mt-0 lg:grid-cols-3">
+	<section>
+		<Rslider bind:min={x.min} bind:max={x.max} bind:values={x.values} label="X" />
+		<Rslider bind:min={y.min} bind:max={y.max} bind:values={y.values} label="Y" />
+		<Rslider bind:min={blur.min} bind:max={blur.max} bind:values={blur.values} label="Blur" />
+	</section>
+	<section class="mx-auto mb-12 flex items-center justify-center text-center lg:mb-0">
+		<ColorPicker
+			bind:hex
+			--cp-border-color="none"
+			--cp-bg-color="none"
+			--cp-input-color="#ddd"
+			--focus-color="#2563eb"
+			components={ChromeVariant}
+			sliderDirection="horizontal"
+			isDialog={false}
+		/>
+	</section>
+	<section class="flex flex-col items-center justify-center">
+		<div class="mb-20 flex h-80 w-80 items-center justify-center rounded-lg bg-gray-300">
+			<p style={textShadowValue} class="text-2xl">John Doe</p>
+		</div>
+		<Code code={textShadowValue} />
+	</section>
+</main>
